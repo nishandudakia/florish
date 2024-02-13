a916dc3d104d2a9e6cb22f2167a441bd6937354e
Jessica-000 <bolu88@outlook.com>
2024-02-13 12:32:53 +0000

    set up the database

--- server/db/setup.sql
+++ server/db/setup.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS events;
 
 CREATE TABLE events(
-event_id INT SERIAL GENERATED ALWAYS AS IDENTITY,
+event_id SERIAL PRIMARY KEY,
 event_name VARCHAR(255) NOT NULL,
 organiser_id INT DEFAULT,
 date DATE,
@@ -12,5 +12,11 @@ location VARCHAR(255),
 accepted_status BOOLEAN DEFAULT FALSE,
 list_of_attendees VARCHAR(500),
 image VARCHAR(600)
-PRIMARY KEY(event_id)
 );
+
+INSERT INTO events
+  (event_name, organiser_id, date, time, number_of_attendees, description, location, accepted_status, list_of_attendees, image)
+VALUES
+   ('puppy spa', 1, 21/05/2024, 8:00, 12, 'A place to pamper your pooches in return for all the love they give', 'Florin grooming salon', True, 'Mason, Scarlet, and more', 'url')
+   ('boxing', 2, 11/04/2024, 9:00, 7, 'Learn to box', 'Florin Gym', True, 'Jason, Ruby, and more', 'url')
+   ('Grannys knitting class', 1, 15/03/2024, 12:00, 18, 'Start a new hobby by taking up knitting', 'Florin community hall', True, 'Melisa, Jenna and more', 'url')
