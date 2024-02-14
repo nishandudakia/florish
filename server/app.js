2b0109937856bd5aec5da5470f468cfad6f12573
lottie-isobel <charlotteeaston@hortmail.co.uk>
2024-02-14 11:26:07 +0000

    add logger

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const logger = require("./middlewares/logger");
 const userRouter = require("./routers/userRouter");
 const app = express();
+app.use(logger);
 app.use(express.json());
 app.use("/users", userRouter);
 
