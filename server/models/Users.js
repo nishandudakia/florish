e9bb7e09be8aad949bb782f7505f036b177e6635
lottie-isobel <charlotteeaston@hortmail.co.uk>
2024-02-14 09:47:16 +0000

    getAll and createUser models created

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,27 +1,61 @@
+const db = require("../db/connect");
+
 class User {
-    constructor(
-      user_id,
+  constructor(
+    user_id,
+    first_name,
+    last_name,
+    user_name,
+    email_address,
+    is_council,
+    council_id
+  ) {
+    this.user_id = user_id;
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.user_name = user_name;
+    this.email_address = email_address;
+    this.is_council = is_council;
+    this.council_id = council_id;
+  }
+  static async getAll() {
+    const response = await db.query("SELECT * FROM User;");
+    if (response.rows.length === 0) {
+      throw new Error("No users available;");
+    } else {
+      return response.rows.map(u => new User(u));
+    }
+  }
+
+  static async createUser(data) {
+    const {
       first_name,
       last_name,
       user_name,
       email_address,
       is_council,
-      council_id
-    ) {
-      this.user_id = user_id;
-      this.first_name = first_name;
-      this.last_name = last_name;
-      this.user_name = user_name;
-      this.email_address = email_address;
-      this.is_council = is_council;
-      this.council_id = council_id;
-    }
-    static async getAll() {
-      const response = await db.query("SELECT * FROM User;");
-      if (response.rows.length === 0) {
-        throw new Error("No users available;")
-      } else {
-        return response.rows.map(u => new User(u));
-      }
+      council_id,
+    } = data;
+
+    const existingUser = await db.query(
+      "SELECT user_name FROM users WHERE LOWER(user_name)=LOWER($1)",
+      [user_name]
+    );
+    if (existingUser.rows.length === 0) {
+      const response = await db.query(
+        "INSERT INTO users(first_name, last_name, user_name, email_address, is_council, council_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;",
+        [
+          first_name,
+          last_name,
+          user_name,
+          email_address,
+          is_council,
+          council_id,
+        ]
+      );
+      return new User(response.rows[0]);
+    } else {
+      throw new Error("This username already exists!");
     }
   }
+}
